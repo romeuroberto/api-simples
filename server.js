59f9693482458f630d858ba3100e614c9405efda
romeuroberto <robertoromeu2009@gmail.com>
2022-05-19 22:33:15 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express =require('express');
+const app = express();
+const data = require("./data.json")
+
+app.use(express.json())
+
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+
+app.post("/clients/:id", function(req, res){});
+app.put("/clients/:id", function(req, res){});
+app.delete("/clients/:id", function(req, res){});
+
+app.listen(3000, function(){
+    console.log("Server is Running");
+});
